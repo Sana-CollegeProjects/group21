e8b96a7f924498664f44e8754fc88108a9bd2003
Dany <rasermax@gmail.com>
2017-03-29 20:57:03 +0000

    Starting to add to cart

---
+++ my-app/www/store_items.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+
+  if(isset($_POST['item_src']))
+  {
+    $_SESSION['name'][]=$_POST['item_name'];
+    $_SESSION['price'][]=$_POST['item_price'];
+    $_SESSION['brand'][]=$_POST['item_brand'];
+    echo count($_SESSION['name']);
+    exit();
+  }
+
+?>
